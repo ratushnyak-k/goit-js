e9d3d424976814d60f0ce4a1fcafe4b457b9e4e9
Kostya <konstantin.ratushniak@yola.com>
2018-10-24 19:45:43 +0300

    add logical-operatiors

---
+++ module-1/logical-operators.js
@@ -0,0 +1,25 @@
+console.log(`1 === 1 //`, 1 === 1);
+console.log(`1 === '1' //`, 1 === '1');
+console.log(`1 == '1' //`, 1 == '1');
+
+console.log(`false === 0 //`, false === 0);
+console.log(`false == 0 //`, false == 0);
+console.log(`'hello'.length === 5 //`, 'hello'.length === 5);
+console.log(`'hello'.length === '5' //`, 'hello'.length == '5');
+console.log(`''.length === false //`, ''.length === false);
+console.log(`''.length == false //`, ''.length == false);
+
+console.log('5 > 6 //', 5 > 6);
+console.log('5 < 6 //', 5 < 6);
+console.log('5 >= 6 //', 5 >= 6);
+console.log('5 <= 6 //', 5 <= 6);
+
+console.log('6 <= 6 //', 6 <= 6);
+console.log('6 >= 6 //', 6 >= 6);
+
+console.log(`5 !== 6 //`, 5 !== 6);
+console.log(`5 !== 5 //`, 5 !== 5);
+
+
+console.log(`!'hello'.length //`, !'hello'.length)
+console.log(`!''.length //`, !''.length)
