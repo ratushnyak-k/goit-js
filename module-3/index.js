185ece3de6778e69c3d1c56bda76d00a9a7bfcbc
Kostya <konstantin.ratushniak@yola.com>
2018-11-09 20:56:46 +0200

    update 3 module

--- module-3/index.js
+++ module-3/index.js
@@ -1,5 +1,10 @@
+let userName;
+let color;
+
 function loginAs() {
-  const userName = prompt('Enter your name.');
+  userName = prompt('Enter your name.');
+  color = getRandomColor();
+
   if (userName) {
     document.getElementById('user-name').innerText = userName;
     document.getElementById('message').removeAttribute('disabled');
@@ -8,12 +13,29 @@ function loginAs() {
   }
 }
 
+function generateTemplate(message) {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'message-container');
+  div.innerHTML = `<div class="message" style="border-color: ${color};">
+                        <div>${message}</div>
+                        <div class="message-user-name">${userName}</div>
+                        <div class="date">${new Date().toLocaleString()}</div>
+                     </div>`;
+  return div;
+}
+
 function writeMessage() {
   const message = prompt('Write a message here!');
   if (message) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'message-container');
-    div.innerHTML = `<div class="message">${message}</div>`;
-    document.getElementById('chat-container').prepend(div);
+    document.getElementById('chat-container').prepend(generateTemplate(message));
   }
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
