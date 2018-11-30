8ed28c32dce62631b0adad08dac35bfe3b676faa
Kostya <konstantin.ratushniak@yola.com>
2018-11-30 08:27:59 +0200

    update 4, 5 module

---
+++ module-6/functional.js
@@ -0,0 +1,30 @@
+function Machine() {
+  const power = 1000;
+
+  this.enable = false;
+  this.color = 'white';
+
+  this.turnOn = function() {
+    this.enable = true;
+  };
+  this.turnOff = function() {
+    this.enable = false;
+  };
+}
+
+function CoffeeMachine() {
+  Machine.apply(this, );
+  this.waterTank = 0;
+}
+
+function Fridge() {
+  Machine.call(this, );
+  this.capacity = '100%';
+}
+
+const m = new Machine();
+const coffee = new CoffeeMachine();
+const fridge = new Fridge();
+console.log(m);
+console.log(coffee);
+console.log(fridge);
---
+++ module-6/prototype.js
@@ -0,0 +1,29 @@
+function Machine(power) {
+  this.enable = false;
+  this.color = 'white';
+  this._power = 1000;
+}
+
+Machine.prototype.turnOn = function() {
+  this.enable = true;
+};
+
+Machine.prototype.turnOff = function() {
+  this.enable = false;
+};
+
+function CoffeeMachine(...args) {
+  Machine.apply(this, args);
+  this.waterTank = 0;
+}
+
+CoffeeMachine.prototype = Object.create(Machine.prototype);
+CoffeeMachine.prototype.constructor = CoffeeMachine;
+
+function Fridge(power) {
+  Machine.call(this, power);
+  this.capacity = '100%';
+}
+
+Fridge.prototype = Object.create(Fridge.prototype);
+Fridge.prototype.constructor = Fridge;
